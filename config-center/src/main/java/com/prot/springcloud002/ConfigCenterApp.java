86e41e23fe7f1053e11349bddefcff053947c2a2
George Zhou <pengcheng.zhou@gmail.com>
2020-08-12 01:10:03 -0400

    add config-server on 8060

---
+++ config-center/src/main/java/com/prot/springcloud002/ConfigCenterApp.java
@@ -0,0 +1,13 @@
+package com.prot.springcloud002;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigCenterApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigCenterApp.class, args);
+    }
+}
